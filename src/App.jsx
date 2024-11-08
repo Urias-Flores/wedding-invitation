111521b1c377cb3108f54f7103d41b5232db7f74
Urias-Flores <floresmeza808@gmail.com>
2024-11-07 20:01:18 -0600

    Scroll bug resolved, adding others

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
       <main>
         <section>
           <h1 className="names">
-            <p className="names__title">Madai</p>
+            <p className="names__title">Kenneth</p>
             <p className="names__title">&</p>
-            <p className="names__title">Keneth</p>
+            <p className="names__title">Madai</p>
           </h1>
 
           <div className="timer">
@@ -65,7 +65,16 @@ function App() {
             <h3>Ceremonia</h3>
             <p>Orillas de la playa, Tela, Atlantida</p>
             <p>05:00 PM</p>
-
+            <iframe
+              className="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.442887002711!2d-87.46284293893538!3d15.78058430071304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f687dfea8faee6b%3A0x175b36df64c2542e!2sTela%2C%20Atl%C3%A1ntida!5e0!3m2!1ses-419!2shn!4v1731028431188!5m2!1ses-419!2shn" 
+              width="400" 
+              height="300" 
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+            >
+            </iframe>
           </div>
 
           <div>
@@ -73,8 +82,16 @@ function App() {
 
             <p>Cabaña contiguo a Hotel XXXXXX, Tela, Atlantida</p>
             <p>08:00 PM</p>
-
-            
+            <iframe
+              className="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.442887002711!2d-87.46284293893538!3d15.78058430071304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f687dfea8faee6b%3A0x175b36df64c2542e!2sTela%2C%20Atl%C3%A1ntida!5e0!3m2!1ses-419!2shn!4v1731028431188!5m2!1ses-419!2shn" 
+              width="400" 
+              height="300" 
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+            >
+            </iframe>
           </div>
         </section>
 
