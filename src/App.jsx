440b25c02661ee799cf91b8ba14af03b4466733b
Urias-Flores <floresmeza808@gmail.com>
2024-11-07 18:56:38 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <section>
+          <h1 className="names">
+            <p className="names__title">Madai</p>
+            <p className="names__title">&</p>
+            <p className="names__title">Keneth</p>
+          </h1>
+
+          <div className="timer">
+            <div className="timer__element">
+              <p>150</p>
+              <p>dias</p>
+            </div>
+
+            <div className="timer__element">
+              <p>14</p>
+              <p>horas</p>
+            </div>
+
+            <div className="timer__element">
+              <p>30</p>
+              <p>minutos</p>
+            </div>
+
+            <div className="timer__element">
+              <p>6</p>
+              <p>segundos</p>
+            </div>
+          </div>
+
+          <h2 className="wewedding">¡Nos Casamos!</h2>
+
+          <p className="verse">
+            Y nosotros hemos conocido y creído el amor que Dios tiene para con nosotros. Dios es amor; y el que permanece en amor, permanece en Dios, y Dios en él <br />
+            <span>1 Juan 4:16</span>
+          </p>
+        </section>
+
+        <section>
+          <h2 className="wedding-date">05 Abril 2024</h2>
+
+          <h3 className="our-parents">Nuestro padres</h3>
+
+
+          <div className="parents">
+            <h4 className="parents__title">Novia</h4>
+            <p className="parents__name">Coric Esmeralda Flores Osorio</p>
+          </div>
+
+          <div className="parents">
+            <h4 className="parents__title">Novio</h4>
+            <p className="parents__name">José Manuel Aguilar Rivas</p>
+            <p className="parents__name">Fanny Yaquelin Flores Sauceda</p>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3>Ceremonia</h3>
+
+            <p>Orillas de la playa, Tela, Atlantida</p>
+            <p>05:00 PM</p>
+          </div>
+
+          <div>
+            <h3>Recepcion</h3>
+
+            <p>Cabaña contiguo a Hotel XXXXXX, Tela, Atlantida</p>
+            <p>08:00 PM</p>
+          </div>
+        </section>
+
+        <section>
+          <h2>M&K</h2>
+
+          <h3>Confirma tu asistencia</h3>
+
+          <form action="POST">
+            <div>
+              <label htmlFor="name">Nombre</label>
+              <input type="text" placeholder="Escribe tu nombre..."/>
+            </div>
+
+            <div>
+              <label htmlFor="adults">Adultos</label>
+              <button>UP</button>
+              <input type="number" placeholder="0" id="adults" name="adults"/>
+              <button>DOWN</button>
+            </div>
+
+            <div>
+              <label htmlFor="children">Niños</label>
+              <button>UP</button>
+              <input type="number" placeholder="0" id="children" name="children"/>
+              <button>DOWN</button>
+            </div>
+          </form>
+        </section>
+
+        <footer>
+          <p>Copyright 2024.</p>
+        </footer>
+      </main>
+    </>
+  )
+}
+
+export default App
