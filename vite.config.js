cdd053d350090d47701a571185063f91c4fc3181
Urias-Flores <floresmeza808@gmail.com>
2024-11-17 22:22:31 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://wedding-invitation-51gl.onrender.com/api",
+        target: "https://wedding-invitation-51gl.onrender.com",
         changeOrigin: true,
       },
     },
